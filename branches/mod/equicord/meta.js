3dbcc177607a0f7294494daeab1340e27cf0ee0e
suprstarrd <26027089+suprstarrd@users.noreply.github.com>
2025-12-10 19:08:15 -0500

    Update meta.js

--- branches/mod/equicord/meta.js
+++ branches/mod/equicord/meta.js
@@ -3,12 +3,12 @@ import { createWriteStream, mkdirSync } from "fs";
 import { join } from "path";
 import { Writable } from "stream";
 
-export const name = "Equicord";
-export const description = "Injects Equicord (a Vencord fork); This is not an officially supported Vencord install method";
+export const name = "Vencord (but actually)";
+export const description = "Injects your Vencord fork";
 export const incompatibilities = ["betterdiscord", "vencord", "moonlight"];
 
 export async function setup(target, log) {
-	const releaseUrl = "https://github.com/Equicord/Equicord/releases/download/latest/";
+	const releaseUrl = "https://github.com/suprdratts/Vencord/releases/download/latest/";
 
 	mkdirSync(join(target, "equicord-desktop"), { recursive: true });
 
